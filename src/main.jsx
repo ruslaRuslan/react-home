0b8f7c838c34cab929558ec7357aff14d6572e1e
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-27 15:26:58 +0400

    render

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
+
+const rootElem = ReactDOM.createRoot(document.getElementById('root'))
+rootElem.render(
+  <div>
+    <h1>Salam abi</h1>
+    <p>axsaminiz xeyirli ugurlu olsun</p>
+    
+  </div>
+)
+// render -> code to UI -> (user interface)
