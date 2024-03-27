c58c0cd5e80950842135a5a1dc541761bf2fc6cc
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-27 19:44:06 +0400

    minimize, maximize

--- src/components/L-2/for-me/Window/index.jsx
+++ src/components/L-2/for-me/Window/index.jsx
@@ -3,16 +3,21 @@ import "./index.css"
 
 const Window = () => {
     const [isOpen, setIsOpen] = useState(false)
-
+    const [scale, setScale] = useState(1)
     return (
         <>
             <button
                 onClick={() => setIsOpen(true)}
             >open window</button>
-         {   isOpen && <div className="window">
+            {isOpen && <div style={{
+                transform: `scale(${scale})`
+            }} className="window">
                 <div className="window_btn_container">
                     <button
-                    onClick={()=>{setIsOpen(false)}}
+                        onClick={() => setScale(0.5)}
+                    >{scale===1 ? "minimize": "maximize"}</button>
+                    <button
+                        onClick={() => { setIsOpen(false) }}
                     >X</button>
 
                 </div>
