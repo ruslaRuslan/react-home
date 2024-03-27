4800b8c37a643bbecb9cb0bb58945a7077444dbd
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-27 15:28:48 +0400

    render-i a-ya menimsetdik

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,23 @@ import './index.css'
 
 
 
-const rootElem = ReactDOM.createRoot(document.getElementById('root'))
-rootElem.render(
-  <div>
-    <h1>Salam abi</h1>
-    <p>axsaminiz xeyirli ugurlu olsun</p>
-    
-  </div>
-)
+// const rootElem = ReactDOM.createRoot(document.getElementById('root'))
+// rootElem.render(
+//   <div>
+//     <h1>Salam abi</h1>
+//     <p>axsaminiz xeyirli ugurlu olsun</p>
+
+//   </div>
+// )
 // render -> code to UI -> (user interface)
+
+// -------------------------------------------------------------------------------------------------------------
+
+const rootElem = ReactDOM.createRoot(document.getElementById('root'))
+const a = <div>
+  <h1>Salam abi</h1>
+  <p>axsaminiz xeyirli ugurlu olsun</p>
+</div>
+rootElem.render(
+a
+)
