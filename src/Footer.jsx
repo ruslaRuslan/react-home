2c2b004fe7c61b2157f647b66305810629ca4925
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-27 16:17:32 +0400

    Home.jsx-i Footer.jsx-de cagirdim

---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <div>
+            <a href="/">Home page</a>
+            <h2>loqo</h2>
+        </div>
+    )
+}
+
+export default Footer;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,10 @@
+import Footer from "./Footer"
+
 function Home() {
     return (
         <div>
             <h1>I'm home page</h1>
+            <Footer />
         </div>
     )
 }
