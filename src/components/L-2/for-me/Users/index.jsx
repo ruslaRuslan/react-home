c90eff885aca1a00cedb46766d5bccf64997fb00
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-27 20:51:07 +0400

    sade lop ile users-in name ve username-lerini cixartdim

--- src/components/L-2/for-me/Users/index.jsx
+++ src/components/L-2/for-me/Users/index.jsx
@@ -2,11 +2,35 @@ import axios from "axios"
 import { useState } from "react"
 
 const url = `https://jsonplaceholder.typicode.com/users`
+// let componentYarandimi = false
+// const Users = () => {
+//     const [users, setUsers] = useState([])
+
+//     if (componentYarandimi !== true) {
+//         axios.get(url).then(({ data }) => {
+//             setUsers(data);
+//             componentYarandimi = true
+//             // console.log();
+//         })
+//     }
+
+
+//     return (
+//         <>
+//             <h1>salam</h1>
+//         </>
+//     )
+// }
+
+// export default Users
+
+// --------------------------------------------------------------------------------------------------------------
+
 let componentYarandimi = false
 const Users = () => {
     const [users, setUsers] = useState([])
 
-    if (componentYarandimi !== true) {
+    if (componentYarandimi === false) {
         axios.get(url).then(({ data }) => {
             setUsers(data);
             componentYarandimi = true
@@ -17,7 +41,15 @@ const Users = () => {
 
     return (
         <>
-            <h1>salam</h1>
+          {
+           users.map((user)=>{
+            return <div key={user.id} >
+                <h1>{user.username}</h1>
+            <p>{user.name}</p>
+
+            </div>
+           })
+          }
         </>
     )
 }
