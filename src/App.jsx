215e1c36da2294b88bbe80407137da0c580d0f7f
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-03-27 18:57:12 +0400

    belede isleyecekler amma ancax console-de gostereceh cavabi isletmeseh daha yaxsidi

--- src/App.jsx
+++ src/App.jsx
@@ -19,19 +19,47 @@
 
 import { useState } from "react";
 
+// function App() {
+//   // let counter = 0
+//   const [counter, setCounter] = useState(0)
+//   return (
+//     <>
+//       <h1>{counter}</h1>
+//       <button
+//         onClick={() => setCounter(counter + 1)}
+//         >+</button>
+
+//       <button
+//         onClick={() => setCounter(counter - 1)}>
+//         -</button>
+
+//       <button
+//         onClick={() => console.log(counter)}
+//       >console</button>
+
+//     </>
+//   )
+// }
+// export default App;
+
+
 function App() {
   // let counter = 0
-  const [counter, setCounter] = useState(0)
+  let [counter, setCounter] = useState(0)
   return (
     <>
       <h1>{counter}</h1>
       <button
-        onClick={() => setCounter(counter + 1)}>
-        +</button>
+        // onClick={() => setCounter(counter + 1)}
+        onClick={() => counter++}
+
+        >+</button>
 
       <button
-        onClick={() => setCounter(counter - 1)}>
-        -</button>
+        // onClick={() => setCounter(counter - 1)}
+        onClick={() => counter--}
+
+        >-</button>
 
       <button
         onClick={() => console.log(counter)}
